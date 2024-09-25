573732038dd23de52dfd63d1d1c24d8ed60580d1
Fernando Herrera <fherrera@onirica.com>
2024-09-25 08:57:37 +0200

    More changes to simplify build process

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -e
+
 #Set this to MuseScore binary
 MUSE=MuseScore-Studio.AppImage
 USE_MEI=1
@@ -50,50 +52,117 @@ function get_status() {
 	local musical_study=$(echo "$1" | jq ".status_musical_study" -r)
 
 	echo "\\def\\mytexttranscription{$text_transcription}"
-  echo "\\def\\mytextproofreading{$text_proof_reading}"
-  echo "\\def\\mytextvalidation{$text_validation}"
-  echo "\\def\\mymusictranscription{$music_transcription}"
-  echo "\\def\\mymusicproofreading{$music_proof_reading}"
-  echo "\\def\\mymusicvalidation{$music_validation}"
-  echo "\\def\\mypoeticstudy{$poetic_study}"
-  echo "\\def\\mymusicalstudy{$musical_study}"
+	echo "\\def\\mytextproofreading{$text_proof_reading}"
+	echo "\\def\\mytextvalidation{$text_validation}"
+	echo "\\def\\mymusictranscription{$music_transcription}"
+	echo "\\def\\mymusicproofreading{$music_proof_reading}"
+	echo "\\def\\mymusicvalidation{$music_validation}"
+	echo "\\def\\mypoeticstudy{$poetic_study}"
+	echo "\\def\\mymusicalstudy{$musical_study}"
 }
 
-function get_text_part() {
-	local text_transcription=$1
-	local text_comments=$2
-
-	echo "\\section*{\centering\Large{Texto}}"
-	echo "\\centering"
-	echo "\\Large{"
-	cat $text_transcription | while read line; do
+function txt_to_tex() {
+	local file="$1"
+	cat "$file" | while read line; do
 		if [ -z "${line}" ]; then
-			echo -e "\n\\\\vspace{2\\\\baselineskip}\n"
+			echo '!'
 		else
-			echo -e "\n${line}\n"
+			echo " "
+			echo -e -n "$line \\\\\\\\"
 		fi
 	done
+	echo '!'
+}
+
+
+function get_text_part() {
+	local dir="$1"
+	local text_transcription="$2"
+	local text_comments="$3"
+
+	echo "\\section*{\\centering\\LARGE{Texto poético}}"
+	echo "\\begingroup"
+	echo "\\centering"
+	echo "\\Large"
+	echo "\\itshape"
+	echo "\\settowidth{\\versewidth}{xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx}"
+	echo "\\setlength{\\vrightskip}{-3em}"
+
+	echo "\\begin{verse}[\\versewidth]"
+	echo "\\poemlines{5}"
+	length=$(echo "$text_transcription" | jq 'length' -r)
+	if [ "$length" -eq 1 ]; then
+		file=$(echo "$text_transcription" | jq '.[0].file' -r)
+		txt_to_tex "$dir/$file"
+	else
+		for i in `seq 0 $(($length - 1))`; do
+			type=$(echo "$text_transcription" | jq ".[$i].type" -r)
+			file=$(echo "$text_transcription" | jq ".[$i].file" -r)
+			echo -n "\\flagverse{$type} " 
+			txt_to_tex "$dir/$file"
+		done
+	fi
+	echo "\\poemlines{0}"
+	echo "\\end{verse}"
+	echo "\\endgroup"
+
+
 	if [ -f $text_comments ]; then
 		echo "\\section*{\centering\Large{Notas a la edición poética}}"
 		cat $text_comments
 	fi
 }
 
+should_append_text() {
+        local dir="$1"
+        local text_transcription="$2"
+        length=$(echo "$text_transcription" | jq 'length' -r)
+        if [ "$length" -eq 1 ]; then
+                file=$(echo "$text_transcription" | jq '.[0].file' -r)
+        else
+                file=$(echo "$text_transcription" | jq '.[] |select(.type=="coplas").file' -r)
+        fi
+
+        extra_stances=`grep -c "^$" "$dir/$file"`
+        if [ $extra_stances -gt 0 ]; then
+                echo "$dir/$file"
+        fi
+}
+
+
 function get_music_part() {
-	local music_transcription="$1"
-	local text_transcription="$2"
-	local music_comments="$3"
-	local title="$4"
-	local order="$5"
-	local poet="$6"
-	local composer="$7"
+	local dir="$1"
+	local music_transcription="$2"
+	local text_transcription="$3"
+	local music_comments="$4"
+	local title="$5"
+	local order="$6"
+	local poet="$7"
+	local composer="$8"
 
 	rm -f music.pdf
 	if [ -f $music_transcription ]; then
 		if [ "$USE_MEI" = "1" ]; then
-			cat "$music_transcription" | sed -e 's/mei-basic/mei-all/g' | sed -e 's/5\.0+basic/5.0/g' > tmp.mei
-			xsltproc --stringparam title "$title" --stringparam order "$order" --stringparam poet "$poet" --stringparam composer "$composer" pgHead.xsl tmp.mei > tmp-with-header.mei
-			sh mei_to_pdf.sh tmp-with-header.mei "$text_transcription" music.pdf > /dev/null
+			# Fix mei exported from musescore
+			cat "$music_transcription" | sed -e 's/mei-basic/mei-all/g' | sed -e 's/5\.0+basic/5.0/g' | xmlstarlet ed -L -N  mei="http://www.music-encoding.org/ns/mei" -s "//mei:score/mei:scoreDef" -t elem -n "pgHead" > tmp1.mei
+
+			# Generate header from metadata using xslt
+			xsltproc --stringparam title "$title" --stringparam order "$order" --stringparam poet "$poet" --stringparam composer "$composer" pgHead.xsl tmp1.mei > tmp2.mei
+
+			# If there are more coplas, append the text in a <div> element 
+			append_text_file=$(should_append_text "$dir" "$text_transcription")
+			if [ ! -z "${append_text_file}" ]; then
+				# Skip first stanza
+        			starting_line=`grep -n "^$" "$append_text_file" | cut -f1 -d: | head -1`
+				verses=$(tail -n +$(($starting_line + 1)) $append_text_file)
+       				java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform -s:tmp2.mei -xsl:coplas.xsl -o:tmp3.mei text="$verses"
+				mv tmp3.mei final.mei
+			else
+				mv tmp2.mei final.mei
+			fi
+
+			# render the mei file 
+			sh mei_to_pdf.sh final.mei music.pdf > /dev/null
 		else
 			$MUSE --export-to=music.pdf $music_transcription
 		fi
@@ -132,19 +201,19 @@ function get_facsimil() {
 }
 
 
+function generate_tono() {
+	local dir="$1"
+	local count=$2
 
-count=0
-for dir in tonos/02*; do
-	count=$(($count + 1))
 	json=$(cat $dir/def.json)
-  S1=$(echo $json | jq '.s1_pages | join(" ")' -r)
-  S2=$(echo $json | jq '.s2_pages | join(" ")' -r)
-  T=$(echo $json | jq '.t_pages | join(" ")' -r)
-  G=$(echo $json | jq '.g_pages | join(" ")' -r)
-  text=$(echo $json | jq '.text_author' -r)
-  music=$(echo $json |jq '.music_author' -r)
-  title=$(echo $json | jq '.title' -r)
-	text_transcription=$dir/$(echo $json |jq '.text_transcription_file' -r)
+	S1=$(echo $json | jq '.s1_pages | join(" ")' -r)
+	S2=$(echo $json | jq '.s2_pages | join(" ")' -r)
+	T=$(echo $json | jq '.t_pages | join(" ")' -r)
+	G=$(echo $json | jq '.g_pages | join(" ")' -r)
+	text=$(echo $json | jq '.text_author' -r)
+	music=$(echo $json |jq '.music_author' -r)
+	title=$(echo $json | jq '.title' -r)
+	text_transcription=$(echo $json | jq '.text_transcription' -r)
 	text_comments=$dir/$(echo $json |jq '.text_comments_file' -r)
 	if [ "$USE_MEI" = "1" ]; then
 		music_transcription=$dir/$(echo $json |jq '.mei_file' -r)
@@ -166,12 +235,12 @@ for dir in tonos/02*; do
 	printf -v TONO "%02d" $count
 
 	get_titles $count "$music" "$text" > values.tex
-	get_version $text_transcription $text_comments $music_transcription $music_comments >> values.tex
+	get_version "$text_transcription" $text_comments $music_transcription $music_comments >> values.tex
 	get_status "$json" >> values.tex
 
 	get_init > tmp.tex
-	get_text_part $text_transcription $text_comments >> tmp.tex
-	get_music_part "$music_transcription" "$text_transcription" "$music_comments" "$title" "$TONO" "$text" "$music" >> tmp.tex
+	get_text_part "$dir" "$text_transcription" $text_comments >> tmp.tex
+	get_music_part "$dir" "$music_transcription" "$text_transcription" "$music_comments" "$title" "$TONO" "$text" "$music" >> tmp.tex
 
 	get_facsimil "$S1" "$S2" "$T" "$G" > facsimil.tex
 	echo "\\input{facsimil.tex}" >> tmp.tex
@@ -179,6 +248,27 @@ for dir in tonos/02*; do
 	echo "\\end{document}" >> tmp.tex
 
 	mkdir -p output
-	pdflatex -interaction=batchmode -quiet tmp.tex && mv tmp.pdf "output/${TONO} - ${title}.pdf"
-	#rm -f tmp.* facsimil.tex values.tex music.pdf
-done
+	if [ "$USE_MEI" = "1" ]; then
+		cp tmp-with-header.mei "output/${TONO} - ${title}.mei"
+		echo "MEI score: output/${TONO} - ${title}.mei"
+	fi
+
+	pdflatex -interaction=batchmode tmp.tex && mv tmp.pdf "output/${TONO} - ${title}.pdf"
+	echo "PDF file: output/${TONO} - ${title}.pdf"
+
+	#rm -f tmp.* facsimil.tex values.tex music.pdf tmp-with-header.mei
+}
+
+if [[ $# -eq 1 ]]; then
+	if [ -d tonos/"$1"* ]; then
+		generate_tono tonos/"$1"* $1
+	else
+		echo "Not such tono: $1"
+	fi
+else
+	count=0
+	for dir in tonos/*; do
+		count=$(($count + 1))
+		generate_tono "$dir" $count
+	done
+fi
