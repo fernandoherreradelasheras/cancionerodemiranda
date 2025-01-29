c26a59cd7492cfa889a3453a263c0281160cf6e3
Fernando Herrera <fherrera@onirica.com>
2025-01-29 21:07:31 +0100

    Added tono 11. Change styles for headings and allow configuring verovio unit

--- mei_to_pdf.sh
+++ mei_to_pdf.sh
@@ -1,13 +1,13 @@
-
-input="$1"
-output="$2"
+unit="$1"
+input="$2"
+output="$3"
 
 
 
 DIR=`mktemp -d`
 
 
-verovio --unit 8.0 --multi-rest-style auto --mdiv-all -a --mm-output  --min-last-justification 0.0  --mnum-interval 0  --bottom-margin-header 2.5 --page-margin-left 150 --page-margin-right 150 --page-margin-top 150  --page-margin-bottom 150  --header auto --footer encoded --condense auto --breaks smart --breaks-smart-sb 0.01 --no-justification -o $DIR/output.svg "$input"
+verovio --unit "$unit" --multi-rest-style auto --mdiv-all -a --mm-output  --min-last-justification 0.0  --mnum-interval 0  --bottom-margin-header 2.5 --page-margin-left 150 --page-margin-right 150 --page-margin-top 150  --page-margin-bottom 150  --header auto --footer encoded --condense auto --breaks smart --breaks-smart-sb 0.01 --no-justification -o $DIR/output.svg "$input"
 
 svgs2pdf -m "$output" $DIR/*.svg
 
