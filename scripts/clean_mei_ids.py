b1093ae3ef1b30a1931460520997a38323702990
Fernando Herrera <fherrera@onirica.com>
2025-02-05 11:25:15 +0100

    add some script and do batch cleaning of mei files.
    - script to remove unused ids
    - xsl template for renumering measures
    - a runner
    
    I cannot find a way to tall xslt/saxon stop reformatting <elem foo="bar" /> into
    <elem foo="bar"/>, so I added a poor man sed step to revert those
    changes.

---
+++ scripts/clean_mei_ids.py
@@ -0,0 +1,47 @@
+import xml.etree.ElementTree as ET
+import sys
+import shutil
+
+ID = '{http://www.w3.org/XML/1998/namespace}id'
+
+PI_HREF = 'https://music-encoding.org/schema/5.0/mei-basic.rng'
+PI_SCHEMA1 = 'http://relaxng.org/ns/structure/1.0'
+PI_SCHEMA2 = 'http://purl.oclc.org/dsdl/schematron'
+
+MEI_NS = 'http://www.music-encoding.org/ns/mei'
+
+ET.register_namespace('', MEI_NS)
+
+input_file = sys.argv[1]
+
+top = ET.Element(None)
+#top.append(ET.ProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"'))
+top.append(ET.ProcessingInstruction(f'xml-model', f'href="{PI_HREF}" type="application/xml" schematypens="{PI_SCHEMA1}"'))
+top.append(ET.ProcessingInstruction(f'xml-model', f'href="{PI_HREF}" type="application/xml" schematypens="{PI_SCHEMA2}"'))
+
+
+target = ET.TreeBuilder (insert_comments=True, insert_pis=True)
+parser = ET.XMLParser(target=target, encoding='utf-8')
+
+tree = ET.parse(input_file, parser)
+
+idsReferenced = {value[1:] for elem in tree.iter() for name,value in elem.items() if value.startswith("#")}
+print(f'Found {len(idsReferenced)} ids referenced that will be kept: {idsReferenced}')
+for e in [elem for elem in tree.iter() for item in elem.items() if item[0] == ID and item[1] not in idsReferenced]:
+    e.attrib.pop(ID, None)
+
+
+root = tree.getroot()
+root.set("xmlns", MEI_NS)
+top.append(root)
+
+newtree = ET.ElementTree(top)
+
+ET.indent(newtree, space="", level=1)
+ET.indent(tree, space="   ", level=0)
+
+newtree.write(".tmp-clean.mei", xml_declaration=True, method="xml", encoding="UTF-8")
+
+shutil.move(".tmp-clean.mei", input_file)
+
+
