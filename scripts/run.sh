71bd977b7ea667f3fcde26e2cb0c1989eaacf390
Fernando Herrera <fherrera@onirica.com>
2025-02-07 14:44:03 +0100

    update scripts and tono 67

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,36 @@
 set -e
 
-if [[ $# -ne 1 ]]; then
-	echo "Usage: $0 <clean-ids | renumber-measures>"
+
+if [[ $# -ne 2 ]]; then
+	echo "Usage: $0 <clean-ids | renumber-measures | fix-ellisons | all> <[file] | -a>"
 	exit 1
 fi
 
-readarray -t mei_files  <<< $(for d in ../tonos/*; do cat "$d/def.json" | jq -r ".mei_file | select(. != null) | \"$d/\" + . "; done)
+cmd="$1"
+target="$2"
 
-if [ "$1" = "clean-ids" ]; then
-	for file in "${mei_files[@]}"; do
-		python ./clean_mei_ids.py "$file"
-	done
-elif [ "$1" = "renumber-measures" ]; then
-	for file in "${mei_files[@]}"; do
-		java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform "-s:$file" -xsl:./fix_mei_measure_ns.xsl -o:./.tmp_output.mei
-		sed -e 's/\([^ ]\)\/>$/\1 \/>/g' ./.tmp_output.mei > "$file"
-		rm ./.tmp_output.mei
-	done
+if [ "$target" = "-a" ]; then
+	readarray -t mei_files  <<< $(for d in ../tonos/*; do cat "$d/def.json" | jq -r ".mei_file | select(. != null) | \"$d/\" + . "; done)
+else
+	mei_files=("$target")
 fi
+
+script_dir=$(dirname "$0")
+
+
+for file in "${mei_files[@]}"; do
+	if [ "$cmd" = "clean-ids" ] || [ "$cmd" = "all" ]; then
+		echo "Cleaning unused ids from $file"
+		python "$script_dir/clean_mei_ids.py" "$file"
+	fi
+	if [ "$cmd" = "renumber-measures" ] || [ "$cmd" = "all" ]; then
+		echo "renumbering measures from $file"
+		java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform "-s:$file" "-xsl:$script_dir/fix_mei_measure_ns.xsl" -o:./.tmp_output.mei
+		sed -e 's/\([^ ]\)\/>$/\1 \/>/g' ./.tmp_output.mei > "$file"
+	fi
+	if [ "$cmd" = "fix-ellisons" ] || [ "$cmd" = "all" ]; then
+		echo "Fixing missing syl ellisons from $file"
+		sh "$script_dir/fix-ellisons.sh" "$file" > ./.tmp_output.mei
+		mv ./.tmp_output.mei "$file"
+	fi
+done
