cc153dc91cbc5f65e95f0bdc6b65db055e863568
Fernando Herrera <fherrera@onirica.com>
2025-02-07 16:04:06 +0100

    Update tono 65. Set score horizontal justification to true

--- mei_to_pdf.sh
+++ mei_to_pdf.sh
@@ -9,7 +9,8 @@ output_dir=$(dirname "$output")
 DIR=`mktemp -d`
 
 
-verovio --unit "$unit" --multi-rest-style auto --mdiv-all -a --mm-output  --mnum-interval 0  --bottom-margin-header 2.5 --page-margin-left 150 --page-margin-right 150 --page-margin-top 50 --bottom-margin-header 8 --page-margin-bottom 50 --top-margin-pg-footer 4  --header auto --footer encoded --no-justification --breaks auto --condense encoded --min-last-justification 1.0 -o $DIR/output.svg "$input"
+#verovio --unit "$unit" --multi-rest-style auto --mdiv-all -a --mm-output  --mnum-interval 0  --bottom-margin-header 2.5 --page-margin-left 150 --page-margin-right 150 --page-margin-top 50 --bottom-margin-header 8 --page-margin-bottom 50 --top-margin-pg-footer 4  --header auto --footer encoded --no-justification --breaks auto --condense encoded --min-last-justification 1.0 -o $DIR/output.svg "$input"
+verovio --unit "$unit" --multi-rest-style auto --mdiv-all -a --mm-output  --mnum-interval 0  --bottom-margin-header 2.5 --page-margin-left 150 --page-margin-right 150 --page-margin-top 50 --bottom-margin-header 8 --page-margin-bottom 50 --top-margin-pg-footer 4  --header auto --footer encoded --breaks auto --condense encoded --min-last-justification 1.0 -o $DIR/output.svg "$input"
 
 svgs2pdf -m "$output_file" -o "$output_dir" $DIR/*.svg
 
