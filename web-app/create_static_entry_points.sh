f1d094c7fe062cbc3a61356e650e958c99640c9d
Fernando Herrera <fherrera@onirica.com>
2025-02-17 19:39:34 +0100

    Update static entry poitns script to use tono/[id]/index.html
    Looks like browser dont like htmnl named without extension like.
    Fortunately github does seearch for index.html if pointed to a
    directory.

--- web-app/create_static_entry_points.sh
+++ web-app/create_static_entry_points.sh
@@ -6,5 +6,6 @@ mkdir -p dist/{tono,tonos,about}
 cp dist/index.html dist/tonos/index.html
 cp dist/index.html dist/about/index.html
 for i in {1..77}; do
-  cp dist/index.html dist/tono/$i
+  mkdir dist/tono/$i
+  cp dist/index.html dist/tono/$i/index.html
 done
