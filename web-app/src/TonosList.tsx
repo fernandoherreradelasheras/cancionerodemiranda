e3828e113a66c97dcf46ea81bdd9c923fd21889b
Fernando Herrera <fherrera@onirica.com>
2025-02-22 13:26:52 +0100

    simplify the status values

--- web-app/src/TonosList.tsx
+++ web-app/src/TonosList.tsx
@@ -5,52 +5,35 @@ import { Context } from './Context';
 
 
 
-function statusStarted(status: string) {
-    return (status == "in progress" || status == "completed");
-}
-
 function tonoOveralStatus(tono: TonoDef | null) {
     if (tono == null) {
         return null;
     }
 
-    if (tono.status_text_transcription == "completed" &&
-        tono.status_text_proof_reading == "completed" &&
-        tono.status_text_validation == "completed" &&
-        tono.status_music_transcription == "completed" &&
-        tono.status_music_proof_reading == "completed" &&
-        tono.status_music_validation == "completed" &&
-        tono.status_poetic_study == "completed" &&
-        tono.status_musical_study == "completed") {
-
+    if (tono.status_text == "completed" &&
+        tono.status_music == "completed") {
         return (
             <span className="item-completed">completed</span>
         )
-    } else if (statusStarted(tono.status_text_transcription) ||
-        statusStarted(tono.status_text_proof_reading) ||
-        statusStarted(tono.status_text_validation) ||
-        statusStarted(tono.status_music_transcription) ||
-        statusStarted(tono.status_music_proof_reading) ||
-        statusStarted(tono.status_music_validation) ||
-        statusStarted(tono.status_poetic_study) ||
-        statusStarted(tono.status_musical_study)) {
-        return (
-            <span className="item-in-progress">in progress</span>
-        )
-    } else {
+    } else if (tono.status_text == "not started" &&
+        tono.status_music == "not started") {
         return (
             <span className="item-not-started">not started</span>
         )
+    } else {
+        return (
+            <span className="item-in-progress">in progress</span>
+
+        )
     }
 }
-
 
 
 function tonoHasMusic(tono: TonoDef | null) {
     if (tono == null) {
         return ""
     } else {
-        return (statusStarted(tono['status_music_transcription']) && tono['mei_file'] != undefined && tono['mei_file'] != '');
+        return (tono['mei_file'] != undefined && tono['mei_file'] != '');
     }
 }
 
@@ -58,8 +41,7 @@ function tonoHasText(tono: TonoDef | null) {
     if (tono == null) {
         return ""
     } else {
-        return (statusStarted(tono['status_text_transcription']) &&
-            tono.text_transcription != undefined && tono.text_transcription.length > 0)
+        return (tono.text_transcription != undefined && tono.text_transcription.length > 0)
     }
 }
 
@@ -106,8 +88,7 @@ const TonoItem = ({ tono, index }: { tono: TonoDef, index: number }) => {
 const TonosList = () => {
 
 
-    const { definitions } = useContext(Context)
-    
+    const { definitions } = useContext(Context)    
 
     const listItems = definitions.map((tono: TonoDef, index: number) => <TonoItem tono={tono} index={index} key={index}  />);
 
