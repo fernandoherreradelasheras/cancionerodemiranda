1293e3afe65a99d21cee463046d0fda90c253c74
Fernando Herrera <fherrera@onirica.com>
2025-02-26 14:47:16 +0100

    Change the mp3 player highlighting

--- web-app/src/svgutils.tsx
+++ web-app/src/svgutils.tsx
@@ -1,6 +1,8 @@
 
 
-export const SVG_STYLE_RULES = [1, 2, 3, 4, 5].map(i=> `.staff[data-n="${i}"] { --high: url(#highlighting-${i}); }`).join('\n') 
+const staffHighlightColors = ["#8e0000", "#227710", "#5500aa", "#e9227a", "#fa8072", "#11ddff",]
+
+export const SVG_STYLE_RULES = [1, 2, 3, 4, 5].map(i=> `.staff[data-n="${i}"] { --high: url(#highlighting-${i}); --hgcolor: ${staffHighlightColors[i-1]} }`).join('\n') 
 
 
 
@@ -10,19 +12,24 @@ const svgFilter = (id: string, color: string) =>
         </feFlood>
         <feGaussianBlur in="SourceAlpha" result="blur-out" stdDeviation="50" />
         <feOffset in="blur-out" result="the-shadow" />
+        <feColorMatrix in="the-shadow" result="color-out" type="matrix"
+            values="0 0 0 0   0
+            0 0 0 0   0
+            0 0 0 0   0
+            0 0 0 2.0 0"/>
         <feComposite result="drop" in="base" in2="color-out" operator="in" />
         <feBlend in="SourceGraphic" in2="drop" mode="normal" />
     </filter>
 
 
+
 export const SVG_FILTERS = 
 <svg xmlns="http://www.w3.org/2000/svg" overflow="visible" style={{height:"0%", width:"0%"}}>
     <defs>
-        {svgFilter("1", "#8e0000")}
-        {svgFilter("2", "#00ff00")}
-        {svgFilter("3", "#225c5c")}
-        {svgFilter("4", "#e9227a")}
-        {svgFilter("4", "#fa8072")}
-        {svgFilter("5", "#11ddff")}
+        {svgFilter("1", staffHighlightColors[0])}
+        {svgFilter("2", staffHighlightColors[1])}
+        {svgFilter("3", staffHighlightColors[2])}
+        {svgFilter("4", staffHighlightColors[3])}
+        {svgFilter("5", staffHighlightColors[4])}    
     </defs>
 </svg>
