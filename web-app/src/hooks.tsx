8beb24c0c88cddfb54b3599d8085f9567746cf2c
Fernando Herrera <fherrera@onirica.com>
2025-02-28 18:15:32 +0100

    web-app: Show a tooptio with measure number. Remove trailing whitespaces

--- web-app/src/hooks.tsx
+++ web-app/src/hooks.tsx
@@ -3,7 +3,7 @@
 export const installWindowHooks = (window: any, { onMouseEnterMusicNotesMeasureNumber, onMouseLeaveMusicNotesMeasureNumber, onMusicNotesMeasureNumberClick }: {
     onMouseEnterMusicNotesMeasureNumber: (m: number) => void,
     onMouseLeaveMusicNotesMeasureNumber: (m: number) => void,
-    onMusicNotesMeasureNumberClick: (m: number) => void 
+    onMusicNotesMeasureNumberClick: (m: number) => void
 }) => {
     window.onMouseEnterMusicNotesMeasureNumber = onMouseEnterMusicNotesMeasureNumber
     window.onMouseLeaveMusicNotesMeasureNumber = onMouseLeaveMusicNotesMeasureNumber
@@ -27,27 +27,30 @@ export const uninstallWindowHooks = (window: any, { onMouseEnterMusicNotesMeasur
 }
 
 
-export const addMeasureHoversForLatex = (text: string) => 
+export const addMeasureHoversForLatex = (text: string) =>
     text.replace(/<strong>Compás ([0-9]+)<\/strong>/g, "<strong class=\"measure-link\" onmouseover='onMouseEnterMusicNotesMeasureNumber($1)' \
         onmouseleave='onMouseLeaveMusicNotesMeasureNumber($1)' onclick='onMusicNotesMeasureNumberClick($1)'>Compás $1</strong>")
 
-export const getSvgHighlightedMeasureStyle = (measure: number) => 
+export const getSvgHighlightedMeasureStyle = (measure: number) =>
     `[data-n="${measure}"] .staff.bounding-box rect { fill:blue;stroke:pink;stroke-width:100;fill-opacity:0.3;stroke-opacity:0.9; }\n`
 
 
-export const getSvgSelectedMeasureStyle = (measure: number) => 
+export const getSvgSelectedMeasureStyle = (measure: number) =>
     `[data-n="${measure}"] .staff.bounding-box rect { fill:red;stroke:orange;stroke-width:50;fill-opacity:0.4;stroke-opacity:0.8; }\n`
 
 
 // Using the filter on the note bouding box gets the whole lyrics and can be quite confusing
 // But stem, is too thin to get really highlight with the svg filter, so...
-export const getSvgMidiHighlightStyle = (id: string) =>  
+export const getSvgMidiHighlightStyle = (id: string) =>
     `g#${id}.note .notehead { filter: var(--high);   }\n`    +
     `g#${id}.note g.stem path { color: var(--hgcolor); stroke-width: 50;  }\n`
 
 
-
-export const getSvgEdirtorialHighlightStyle = (id: string, _: string) => 
+export const getSvgEdirtorialHighlightStyle = (id: string, _: string) =>
     `g#${id}.note > .note.boundingbox > rect { fill:blue;stroke:rga(172, 32, 32);stroke-width:3;fill-opacity:0.1;stroke-opacity:0.9; }\n`
 
 export const getVerovioSvgExtraAttributes = () =>  [ "measure@n", "staff@n", "clef@corresp" ]
+
+export const SVG_STAFF_CSS_SELECTOR = 'svg g.staff.bounding-box'
+export const getStaffTooltipContent = (render: { content: string | null; activeAnchor: HTMLElement | null }) =>
+    <span>Compás {render.activeAnchor?.parentElement?.parentElement?.getAttribute("data-n")}</span>
--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import 'react-tooltip/dist/react-tooltip.css'
 import './index.css'
+
 import App from './App.tsx'
 
 
