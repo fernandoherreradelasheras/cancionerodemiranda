01a7b217d89af4dbe7fb12d68eed74e934d54846
Fernando Herrera <fherrera@onirica.com>
2025-03-01 15:56:32 +0100

    Update text in about page

--- web-app/src/About.tsx
+++ web-app/src/About.tsx
@@ -1,16 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+const REPO_URL = "https://github.com/fernandoherreradelasheras/cancionerodemiranda"
 
 const About = () => (
-    <section>
+    <section className="about">
         <header className="main">
             <h1>Acerca del proyecto</h1>
         </header>
 
         <span className="image main"><img src="header1.jpg" alt="" /></span>
-        <p>El cancionero de Miranda se encuentra actualmente en fase de edición. Esta edición, al requerir la reconstrucción de la voz perdida del alto, require un trabajo superior al habitual.
-            Todo este trabajo de transcripción y edición se está realizando en el repositorio público del proyecto <a href="https://github.com/fernandoherreradelasheras/cancionerodemiranda">https://github.com/fernandoherreradelasheras/cancionerodemiranda</a>.</p>
+        <p>Estamos trabajando en la edición del Cancionero de Miranda. Esta edición, al incluir
+            la reconstrucción de la voz perdida del alto, require un trabajo superior al habitual.
+            Todo este trabajo de transcripción, reconstrucción y edición se está realizando en el
+            repositorio público del proyecto <a href={REPO_URL}>{REPO_URL}</a>.</p>
 
-        <p>Hasta que la edición esté lista, esta web permite consultar el progreso de la edición de cada uno de los tonos que componen el cancionero. </p>
-        <p>A través del menú de la izquierda se puede acceder a las textos y partituras en desarrollo, tanto en versión web como en versiones imprimibles similares al aspecto que tendrán en la edición final. </p>
+        <p>Hasta que la edición esté lista, esta web permite consultar el progreso de cada uno de
+            los tonos que componen el cancionero, así como ver las versiones <b>de trabajo</b>
+            de los textos y partituras correspondientes. En muchos casos, el visor de la partituras
+            permite también reproducir una interpretación generada por software con el fin de facilitar
+            el trabajo de ajuste silábico. Estas interpretaciones por software a veces usan una afinación
+            inferior a los 415Hz o 392Hz habituales para adaptarse mejor a los rangos óptimos de los bancos
+            de voces utilizados.</p>
+
+        <p>El <NavLink to="/tonos">listado de tonos</NavLink>, accesible también desde el menú la
+        barra lateral de la izquierda, muestra el estado de cada uno de los tonos y permite acceder
+        a textos y partituras, e imágenes de los manuscritos. Además se incluye una versión imprimible
+        de todo el contenido, similar en aspecto al de una posible edición en papel.</p>
     </section>
 )
 
