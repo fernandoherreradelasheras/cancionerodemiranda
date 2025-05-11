f51e9cb2a98d807f348a58ee6c4cb3c4499517a7
Fernando Herrera <fherrera@onirica.com>
2025-05-11 10:54:51 +0200

    update about page text

--- web-app/src/About.tsx
+++ web-app/src/About.tsx
@@ -10,18 +10,30 @@ const About = () => (
         </header>
         <Image width="70%" src="/header1.jpg" preview={false}></Image>
 
-        <p>Estamos trabajando en la edición del Cancionero de Miranda. Esta edición, al incluir
-            la reconstrucción de la voz perdida del alto, require un trabajo superior al habitual.
-            Todo este trabajo de transcripción, reconstrucción y edición se está realizando en el
-            repositorio público del proyecto <a href={REPO_URL}>{REPO_URL}</a>.</p>
+        <p>Estamos trabajando en la edición del Cancionero de Miranda. Todo el trabajo de
+            estudio, redacción, transcripción y reconstrucción se está realizando en abierto
+            en el repositorio público del proyecto <a href={REPO_URL}>{REPO_URL}</a>.</p>
 
-        <p>Hasta que la edición esté lista, esta web permite consultar el progreso de cada uno de
-            los tonos que componen el cancionero, así como ver las versiones <b>de trabajo</b>
-            de los textos y partituras correspondientes. En muchos casos, el visor de la partituras
-            permite también reproducir una interpretación generada por software con el fin de facilitar
-            el trabajo de ajuste silábico. Estas interpretaciones por software a veces usan una afinación
-            inferior a los 415Hz o 392Hz habituales para adaptarse mejor a los rangos óptimos de los bancos
-            de voces utilizados.</p>
+        <p>En este proyecto se están utilizando herramientas novedosas para poder ofrecer una edición
+        multimodal en papel y digital que permita disfrutar de la música y el texto de la forma que mejor
+        se adapte a cada usuario ya sean intérpretes, invetigadores, musicólogos o simplemente aficionados
+        a la música antigua.</p>
+
+        <p>La visualización de las partituras digitales permite configurar varios aspectos como las transposiciones
+            de claves altas, modernización de las claves, normalización de música ficta, configuración del número de versos a
+            mostrar, etc.</p>
+
+        <p>Otra novedad interesante es el uso de un reproductor de audio con interpretaciones de los tonos realizadas
+            por software. Estas versiones realizadas con cantantes virtuales no solo facilita el trabajo de edición en temas
+            como el ajuste silábico sino que también pueden servir como una primera aproximación a la música cantada. Este
+            reproductor, además de mostrar las notas que están sonando en cada momento tiene un modo inmersivo que muestra
+            la partitura con una animación de sroll infinito.</p>
+
+        <p>Para la reconstrucción de la voz perdida de alto, además del trabajo tradicional de recomposición, en el que están colaborando
+            diversos musicólogos, se está experimentando con el uso de diferentes modelos de intelegencia artifical.
+            La tarea de completar una voz que se ha de ceñir a unas reglas específicas pero a la vez considerar casos fuera de las
+            pautas establecidas es un interesante retos para estas tecnologías. Varias de estas reconsutrucciones están ya disponibles, incluyendo
+            el audio resultante de mezclar las diversas opciones.</p>
 
         <p>El <NavLink to="/tonos">listado de tonos</NavLink>, accesible también desde el menú la
         barra superior, muestra el estado de cada uno de los tonos y permite acceder
