542e37705b483012662270a32ff04232f96043fd
Fernando Herrera <fherrera@onirica.com>
2025-07-16 14:52:55 +0200

    Drop mei2hum from humlib and use convert21 for convert mei to humdrum

--- scripts/add_dissonant_analysis.sh
+++ scripts/add_dissonant_analysis.sh
@@ -2,10 +2,16 @@ TMP=`mktemp -d`
 SCRIPTDIR=`dirname "$0"`
 
 echo "Performing dissonant analysis for file $1"
+
+python $SCRIPTDIR/filter_editorials.py "$1" > $TMP/filtered.mei
+python3 -m converter21 -f mei -t humdrum $TMP/filtered.mei  $TMP/filtered.krn 
+
 echo '!!!filter: dissonant' > $TMP/dissonant-analysis.krn
-python $SCRIPTDIR/filter_editorials.py "$1" | mei2hum |  extract -i '**kern' >> $TMP/dissonant-analysis.krn
+cat $TMP/filtered.krn | extract -i '**kern' >> $TMP/dissonant-analysis.krn
+
 verovio $TMP/dissonant-analysis.krn  -t mei -o $TMP/dissonant-analysis.mei
 java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform -s:$TMP/dissonant-analysis.mei "-xsl:$SCRIPTDIR/fix_mei_measure_ns.xsl" -o:$TMP/dissonant-analysis-with-n.mei
+
 python "$SCRIPTDIR/merge_harm.py" "$1" "$TMP/dissonant-analysis-with-n.mei" "$2"
 if [ $? != 0 ]; then
   echo "error merging harm elements. Kept temporal files at $TMP"
