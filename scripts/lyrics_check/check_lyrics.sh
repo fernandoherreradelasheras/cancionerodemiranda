4c3c6bd274cf4477c2c4eec253ac740ad295f129
Fernando Herrera <fherrera@onirica.com>
2025-07-29 12:25:06 +0200

    Lyrics fixes for tono 3

--- scripts/lyrics_check/check_lyrics.sh
+++ scripts/lyrics_check/check_lyrics.sh
@@ -6,7 +6,8 @@ mkdir $TMP/filtered
 mkdir $TMP/stanzas
 
 echo "Performing check lyrics for file $1"
-python "$SCRIPTDIR/mei_lyrics_extractor.py" -o $TMP/raw --simple "$1"
+xmlstarlet ed -P -N mei="http://www.music-encoding.org/ns/mei"   -d '//mei:app[@type="voice_reconstruction"]' "$1" > $TMP/initial.mei
+python "$SCRIPTDIR/mei_lyrics_extractor.py" -o $TMP/raw --simple "$TMP/initial.mei"
 for i in $TMP/raw/*.txt; do
   name=`basename "$i"`
   cat "$i" | python "$SCRIPTDIR/remove_repetitions_from_voice.py" > "$TMP/filtered/$name"
