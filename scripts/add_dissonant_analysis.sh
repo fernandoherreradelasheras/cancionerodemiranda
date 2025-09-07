fc260a30f941bf6e1fbb153338a47389d82efcb8
Fernando Herrera <fherrera@onirica.com>
2025-09-08 01:46:50 +0200

    update script to add harm elements inplace

--- scripts/add_dissonant_analysis.sh
+++ scripts/add_dissonant_analysis.sh
@@ -3,6 +3,8 @@ SCRIPTDIR=`dirname "$0"`
 
 echo "Performing dissonant analysis for file $1"
 
+cp "$1" "$TMP/backup.mei"
+
 xmlstarlet ed  -P -N mei="http://www.music-encoding.org/ns/mei"  -d '//mei:app[mei:rdg[@type="dissonant_analysis"]]' "$1" > $TMP/clean.mei
 python $SCRIPTDIR/filter_editorials.py $TMP/clean.mei > $TMP/filtered.mei
 python3 -m converter21 -f mei -t humdrum $TMP/filtered.mei  $TMP/filtered.krn 
@@ -13,11 +15,13 @@ cat $TMP/filtered.krn | extract -i '**kern' >> $TMP/dissonant-analysis.krn
 verovio $TMP/dissonant-analysis.krn  -t mei -o $TMP/dissonant-analysis.mei
 java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform -s:$TMP/dissonant-analysis.mei "-xsl:$SCRIPTDIR/fix_mei_measure_ns.xsl" -o:$TMP/dissonant-analysis-with-n.mei
 
-python "$SCRIPTDIR/merge_harm.py" "$TMP/clean.mei" "$TMP/dissonant-analysis-with-n.mei" "$2"
+python "$SCRIPTDIR/merge_harm.py" "$TMP/clean.mei" "$TMP/dissonant-analysis-with-n.mei" "$TMP/merged_analysis"
 if [ $? != 0 ]; then
   echo "error merging harm elements. Kept temporal files at $TMP"
   exit 1
 fi
 
+python $SCRIPTDIR/xml_reindent.py  "$TMP/merged_analysis" -o "$1"
+
 #rm -rf $TMP
-echo "New file with merged analysis as <harm> elements: '$2'"
+echo "New file with merged analysis as <harm> elements: '$1'"
