a8174c5f7dce984f9f0c5dd36cb4dae7b76ceab8
Fernando Herrera <fherrera@onirica.com>
2025-09-08 17:42:49 +0200

    web-app: Update status and dashboard

--- web-app/src/TonoView.tsx
+++ web-app/src/TonoView.tsx
@@ -13,7 +13,7 @@ const STICKY_HEADER_HEIGHT = 48
 const USE_VIRTUAL_UNITS = true
 
 
-library.add(faMusic, faFilePdf, faFileImage )
+library.add(faMusic, faFilePdf, faFileImage)
 
 type Section = {
     label: string,
@@ -21,31 +21,32 @@ type Section = {
 }
 
 const getProgressFromTextStatus = (status?: TextStatus) => {
-    switch(status) {
+    switch (status) {
         case undefined: return { value: 5, text: "sin comenzar" }
         case "not started": return { value: 5, text: "sin comenzar" }
-        case "raw transcription": return  { value: 25, text: "transcripción en progreso" }
-        case "transcription completed": return  { value: 50, text: "transcripción completa" }
-        case "reviewed": return  { value: 75, text: "revisado" }
+        case "raw transcription": return { value: 25, text: "transcripción en progreso" }
+        case "transcription completed": return { value: 70, text: "transcripción completa" }
+        case "reviewed": return { value: 80, text: "revisado" }
     }
-    return  { value: 100, text: "completado" }
+    return { value: 100, text: "completado" }
 
 }
 
 const getProgressFromMusicStatus = (status?: MusicStatus) => {
-    switch(status) {
+    switch (status) {
         case undefined: return { value: 5, text: "sin comenzar" }
         case "not started": return { value: 5, text: "sin comenzar" }
-        case "raw transcription": return  { value: 20, text: "transcripción en progreso" }
-        case "transcription completed": return  { value: 40, text: "transcripción completa" }
-        case "all voices completed": return { value: 60, text: "todas las voces completas"}
-        case "reviewed": return  { value: 80, text: "revisado" }
+        case "raw transcription": return { value: 20, text: "transcripción en progreso" }
+        case "transcription completed": return { value: 40, text: "transcripción completa" }
+        case "reconstruction started": return { value: 60, text: "todas las voces completas" }
+        case "music completed": return { value: 70, text: "música completa" }
+        case "reviewed": return { value: 80, text: "revisado" }
     }
-    return  { value: 100, text: "completado" }
+    return { value: 100, text: "completado" }
 }
 
 const get_edition_name = (name: string) => {
-    switch(name) {
+    switch (name) {
         case "scholar":
             return "Edición para musicólogos (pdf)"
         case "performer":
@@ -56,7 +57,7 @@ const get_edition_name = (name: string) => {
 }
 
 const get_edition_shortname = (name: string) => {
-    switch(name) {
+    switch (name) {
         case "scholar":
             return "musicólogos"
         case "performer":
@@ -94,15 +95,15 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
 
     const { scoreViewerConfig, status: definitions } = useContext(Context)
 
-    const [scoreSize, setScoreSize] = useState<{width: string, height: string, scrollTo: number | null} | null>(null)
+    const [scoreSize, setScoreSize] = useState<{ width: string, height: string, scrollTo: number | null } | null>(null)
     const [scoreProperties, setScoreProperties] = useState<ScoreProperties | null>(null)
     const [visualizationOptions, setVisualizationOptions] = useState<VisualizationOptions | null>(null)
     const scoreViewerContainerRef = useRef<HTMLDivElement>(null)
 
     const tonoStatus = useMemo(() => definitions && tonoIndex != null ? definitions[tonoIndex] : null, [definitions, tonoIndex])
 
-    const { "value": textStatusValue , "text": textStatusText } = getProgressFromTextStatus(tonoStatus?.status_text)
-    const { "value": musicStatusValue , "text": musicStatusText } = getProgressFromMusicStatus(tonoStatus?.status_music)
+    const { "value": textStatusValue, "text": textStatusText } = getProgressFromTextStatus(tonoStatus?.status_text)
+    const { "value": musicStatusValue, "text": musicStatusText } = getProgressFromMusicStatus(tonoStatus?.status_music)
 
     const scoreViewerRef = useRef<ScoreViewerRef>(null);
 
@@ -129,7 +130,7 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
 
     const onVisualizationOptionsChanged = (changedOptions: VisualizationOptions) => {
         setVisualizationOptions(
-            (visualizationOptions : VisualizationOptions | null) => ({ ...visualizationOptions, ...changedOptions })
+            (visualizationOptions: VisualizationOptions | null) => ({ ...visualizationOptions, ...changedOptions })
         )
     }
 
@@ -150,14 +151,14 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
         return reconstructionTexts.join(", ")
     }, [visualizationOptions])
 
-   const editor = scoreProperties?.editor
-   const reconstruction = scoreProperties?.reconstructionBy
-   const numMeasures = scoreProperties?.numMeasures
-   const title = tonoIndex ? `${scoreViewerConfig?.scores[tonoIndex].title}` : ""
+    const editor = scoreProperties?.editor
+    const reconstruction = scoreProperties?.reconstructionBy
+    const numMeasures = scoreProperties?.numMeasures
+    const title = tonoIndex ? `${scoreViewerConfig?.scores[tonoIndex].title}` : ""
 
-   const sectionItems = useMemo(() => {
+    const sectionItems = useMemo(() => {
         return scoreProperties?.sections?.map((section: Section, index: number) =>
-             <a onClick={() => { onClickSection(section) }}>{`${index + 1}. ${section.label}`}</a>) || null
+            <a onClick={() => { onClickSection(section) }}>{`${index + 1}. ${section.label}`}</a>) || null
     }, [scoreProperties])
 
 
@@ -166,18 +167,18 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
         if (scoreViewerContainerRef.current && !scoreSize) {
             const viewPortHeight = window.innerHeight
             const rect = scoreViewerContainerRef.current.getBoundingClientRect()
-            const availableHeight =  rect.top >= 0 ? Math.floor(viewPortHeight - rect.top) - 6
+            const availableHeight = rect.top >= 0 ? Math.floor(viewPortHeight - rect.top) - 6
                 : Math.floor(viewPortHeight - (window.scrollY + rect.top)) - 6
             if (availableHeight >= MINIMUM_SCORE_HEIGHT) {
                 setScoreSize({
-                    width:  USE_VIRTUAL_UNITS ? "100%" : `${Math.floor(rect.width)}px`,
+                    width: USE_VIRTUAL_UNITS ? "100%" : `${Math.floor(rect.width)}px`,
                     height: USE_VIRTUAL_UNITS ? `${Math.round(availableHeight / viewPortHeight * 100) - 4}vh` : `${availableHeight}px`,
                     scrollTo: null
                 })
             } else {
                 const height = viewPortHeight - STICKY_HEADER_HEIGHT - 6
                 setScoreSize({
-                    width:  USE_VIRTUAL_UNITS ? "100%" : `${Math.floor(rect.width)}px`,
+                    width: USE_VIRTUAL_UNITS ? "100%" : `${Math.floor(rect.width)}px`,
                     height: USE_VIRTUAL_UNITS ? `${height / viewPortHeight * 100}vh` : `${viewPortHeight - STICKY_HEADER_HEIGHT - 6}px`,
                     scrollTo: rect.top + 6,
                 })
@@ -203,58 +204,58 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
     return (
         <div>
 
-        <Row  style={{ justifyContent: "space-between", backgroundColor: "white", padding: "0.2em" }}>
-            <Col xl={{flex: 1 }}
-                 lg={{ flex: 1 }}
-                 md={{ flex: 1 }}
-                 sm={{ flex: 1 }}
-                 xs={{ flex: '50%' }}>
-                    <Typography.Text>Música: {tonoStatus?.music_author}</Typography.Text><br/>
-                    <Typography.Text>Texto: {tonoStatus?.text_author}</Typography.Text><br/>
+            <Row style={{ justifyContent: "space-between", backgroundColor: "white", padding: "0.2em" }}>
+                <Col xl={{ flex: 1 }}
+                    lg={{ flex: 1 }}
+                    md={{ flex: 1 }}
+                    sm={{ flex: 1 }}
+                    xs={{ flex: '50%' }}>
+                    <Typography.Text>Música: {tonoStatus?.music_author}</Typography.Text><br />
+                    <Typography.Text>Texto: {tonoStatus?.text_author}</Typography.Text><br />
                     <Typography.Text>Orgánico: {tonoStatus?.organic}</Typography.Text>
 
                 </Col>
-                <Col xl={{flex: 1}}
-                 lg={{ flex: 1 }}
-                 md={{ flex: 1 }}
-                 sm={{ flex: 1 }}
-                 xs={{ flex: '50%' }}>
+                <Col xl={{ flex: 1 }}
+                    lg={{ flex: 1 }}
+                    md={{ flex: 1 }}
+                    sm={{ flex: 1 }}
+                    xs={{ flex: '50%' }}>
                     <div>Música: {musicStatusText}</div>
-                    <Progress percent={musicStatusValue} steps={9} showInfo={false} strokeColor={progressColors}  />
+                    <Progress percent={musicStatusValue} steps={9} showInfo={false} strokeColor={progressColors} />
                     <div>Texto: {textStatusText}</div>
-                    <Progress percent={textStatusValue} steps={9} showInfo={false} strokeColor={progressColors}  />
+                    <Progress percent={textStatusValue} steps={9} showInfo={false} strokeColor={progressColors} />
                 </Col>
-                <Col xl={{flex: 1}}
-                 lg={{ flex: 1 }}
-                 md={{ flex: 1 }}
-                 sm={{ flex: 1 }}
-                 xs={{ flex: '50%' }}>
-                    { scoreProperties? <div>
-                            <> <Typography.Text>Transcripción: {editor}</Typography.Text><br/> </>
-                            { reconstruction || reconstructionsFromOptions ?
-                                <> <Typography.Text>Reconstrucción: {reconstruction || reconstructionsFromOptions}</Typography.Text><br/> </>: null }
-                            <> <Typography.Text>Num compases: {numMeasures}</Typography.Text> </>
-                        </div> : null }
+                <Col xl={{ flex: 1 }}
+                    lg={{ flex: 1 }}
+                    md={{ flex: 1 }}
+                    sm={{ flex: 1 }}
+                    xs={{ flex: '50%' }}>
+                    {scoreProperties ? <div>
+                        <> <Typography.Text>Transcripción: {editor}</Typography.Text><br /> </>
+                        {reconstruction || reconstructionsFromOptions ?
+                            <> <Typography.Text>Reconstrucción: {reconstruction || reconstructionsFromOptions}</Typography.Text><br /> </> : null}
+                        <> <Typography.Text>Num compases: {numMeasures}</Typography.Text> </>
+                    </div> : null}
                 </Col>
-                <Col xl={{flex: 1 }}
-                 lg={{ flex: 1 }}
-                 md={{ flex: 1 }}
-                 sm={{ flex: 1 }}
-                 xs={{ flex: '50%' }}>
-                    { sectionItems && sectionItems.length > 1 ? <Space direction="vertical">
-                            <Typography.Text>Secciones:</Typography.Text>
-                            {sectionItems}
-                        </Space> : null }
+                <Col xl={{ flex: 1 }}
+                    lg={{ flex: 1 }}
+                    md={{ flex: 1 }}
+                    sm={{ flex: 1 }}
+                    xs={{ flex: '50%' }}>
+                    {sectionItems && sectionItems.length > 1 ? <Space direction="vertical">
+                        <Typography.Text>Secciones:</Typography.Text>
+                        {sectionItems}
+                    </Space> : null}
                 </Col>
-                <Col xl={{flex: 1 }}
-                 lg={{ flex: 1 }}
+                <Col xl={{ flex: 1 }}
+                    lg={{ flex: 1 }}
                     md={{ flex: 1 }}
                     sm={{ flex: 1 }}
                     xs={{ flex: '50%' }}>
                     {tonoIndex != null && tonoStatus?.pdfs && tonoStatus.pdfs.length > 0 ? <Space direction="vertical">
                         <Typography.Text> Versiones para imprimir:</Typography.Text>
                         {tonoStatus.pdfs.map((pdf, index) =>
-                             pdf.name ? <a key={index}  download={get_edition_filename(pdf.name, tonoIndex, title)} href={pdf.url}>{get_edition_name(pdf.name)}</a> : null
+                            pdf.name ? <a key={index} download={get_edition_filename(pdf.name, tonoIndex, title)} href={pdf.url}>{get_edition_name(pdf.name)}</a> : null
                         )}
                     </Space> : null}
                 </Col>
@@ -266,14 +267,14 @@ const TonoView = ({ tonoIndex }: { tonoIndex: number | null }) => {
                     height: "100%", width: "100%"
                 }}>
 
-                { scoreViewerConfig && scoreSize ?
+                {scoreViewerConfig && scoreSize ?
                     <ScoreViewer
                         width={scoreSize?.width}
                         height={scoreSize?.height}
                         config={scoreViewerConfig}
                         ref={scoreViewerRef}
                         onVisualizationOptionsChanged={onVisualizationOptionsChanged}
-                        onScoreAnalyzed={onScoreAnalyzed} /> : null }
+                        onScoreAnalyzed={onScoreAnalyzed} /> : null}
             </div>
         </div>
     )
