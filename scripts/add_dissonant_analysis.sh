174bfbbdc6410d5cf9e098cbc4cd770605cf971d
Fernando Herrera <fherrera@onirica.com>
2025-10-16 22:58:44 +0200

    tono 58: add on-going alto reconstruction

--- scripts/add_dissonant_analysis.sh
+++ scripts/add_dissonant_analysis.sh
@@ -7,12 +7,13 @@ cp "$1" "$TMP/backup.mei"
 
 xmlstarlet ed  -P -N mei="http://www.music-encoding.org/ns/mei"  -d '//mei:app[mei:rdg[@type="dissonant_analysis"]]' "$1" > $TMP/clean.mei
 python $SCRIPTDIR/filter_editorials.py $TMP/clean.mei > $TMP/filtered.mei
+echo "clean MEI without editorials at $TMP/filtered.mei"
 python3 -m converter21 -f mei -t humdrum $TMP/filtered.mei  $TMP/filtered.krn 
 
 echo '!!!filter: dissonant' > $TMP/dissonant-analysis.krn
 cat $TMP/filtered.krn | extract -i '**kern' >> $TMP/dissonant-analysis.krn
 
-verovio -a $TMP/dissonant-analysis.krn  -t mei -o $TMP/dissonant-analysis.mei
+verovio --mdiv-all -a $TMP/dissonant-analysis.krn  -t mei -o $TMP/dissonant-analysis.mei
 python $SCRIPTDIR/filter-mei-dup-ties.py  $TMP/dissonant-analysis.mei --output $TMP/dissonant-analysis-fixed.mei
 java -cp /usr/share/java/saxon/saxon-he.jar net.sf.saxon.Transform -s:$TMP/dissonant-analysis-fixed.mei "-xsl:$SCRIPTDIR/fix_mei_measure_ns.xsl" -o:$TMP/dissonant-analysis-with-n.mei
 
