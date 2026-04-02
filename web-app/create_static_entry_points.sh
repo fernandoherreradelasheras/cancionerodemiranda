d4e6a5116206390c3eb73f7be3855345986baea9
Fernando Herrera <fherrera@onirica.com>
2026-04-02 05:10:41 +0200

    Create a sitemap.xml for gh pages with the post-deploy script

--- web-app/create_static_entry_points.sh
+++ web-app/create_static_entry_points.sh
@@ -11,6 +11,19 @@ for i in {1..77}; do
   cp dist/index.html dist/tono/$i/index.html
 done
 
+echo "Generating sitemap.xml"
+SITEMAP="dist/sitemap.xml"
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $SITEMAP
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $SITEMAP
+echo '  <url><loc>https://cdm.humanoydivino.com/</loc></url>' >> $SITEMAP
+echo '  <url><loc>https://cdm.humanoydivino.com/tonos/</loc></url>' >> $SITEMAP
+echo '  <url><loc>https://cdm.humanoydivino.com/about/</loc></url>' >> $SITEMAP
+echo '  <url><loc>https://cdm.humanoydivino.com/progreso/</loc></url>' >> $SITEMAP
+for i in {1..77}; do
+  echo "  <url><loc>https://cdm.humanoydivino.com/tono/$i/</loc></url>" >> $SITEMAP
+done
+echo '</urlset>' >> $SITEMAP
+
 echo "Copying image assets"
 mkdir -p dist/assets
 cp -v ../assets/* dist/assets
